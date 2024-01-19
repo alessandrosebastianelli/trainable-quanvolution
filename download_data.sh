1da9ab703c9d5a579d66d6998d4126dab8da80ea
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2024-01-19 12:38:01 +0100

    script to download the dataset

---
+++ download_data.sh
@@ -0,0 +1,16 @@
+rm EuroSAT.zip
+rm -r 2750
+rm -r dataset
+
+wget https://madm.dfki.de/files/sentinel/EuroSAT.zip --no-check-certificate
+unzip EuroSAT.zip
+mkdir dataset
+
+mkdir dataset/validation
+mv 2750 EuroSAT
+mv EuroSAT dataset/
+cd dataset
+mv EuroSAT training
+cd ..
+
+python scripts/split_dataset.py
